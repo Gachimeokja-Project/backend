5400a9d71b1d2a36be0cc84318dc2a5e7946d826
dohyunk58 <hedge3x@gmail.com>
2025-07-31 21:33:11 +0900

    Initial Commit

---
+++ src/main/java/com/css/gachimeokja/GachimeokjaApplication.java
@@ -0,0 +1,13 @@
+package com.css.gachimeokja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GachimeokjaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GachimeokjaApplication.class, args);
+    }
+
+}
